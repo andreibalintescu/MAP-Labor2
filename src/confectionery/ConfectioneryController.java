a86e555cf007cb41b3d46f6e95be20eb29f88c73
denysa2004 <149471938+denysa2004@users.noreply.github.com>
2024-11-04 17:44:52 +0200

    ViewMenu method added in the Repository

--- src/confectionery/ConfectioneryController.java
+++ src/confectionery/ConfectioneryController.java
@@ -15,22 +15,4 @@ public class ConfectioneryController {
     }
 
 
-//    public void viewStudents() {
-//        StringBuilder output = new StringBuilder("Available students:\n");
-//        universityService.getAllStudents().forEach(student -> output.append(student.toString()).append("\n"));
-//        System.out.println(output);
-//    }
-//
-//
-//    public void viewEnrolled(Integer courseId) {
-//        StringBuilder output = new StringBuilder("Enrolled students in the course:\n");
-//        universityService.getEnrolledStudents(courseId).forEach(student -> output.append(student.toString()).append("\n"));
-//        System.out.println(output);
-//    }
-//
-//
-//    public void deleteCourse(Integer courseId) {
-//        universityService.removeCourse(courseId);
-//        System.out.println("Removed course " + courseId);
-//    }
-}
+   }
